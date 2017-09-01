76c10ab86dfa3a6ce87c8a99766401d1078a812a
shovalsolo <shovalso@gmail.com>
2017-09-01 13:44:33 -0400

    Map loop and form
    Map Loop
    and Form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-//import {getEncounters} from './request.js';
 import axios from 'axios'; 
 import $ from 'jquery';
 
+var ReactRouter = require('react-router-dom');
+var Router = ReactRouter.BrowserRouter;
+var Route = ReactRouter.Route;
 
 
 class App extends Component {
@@ -12,39 +14,15 @@ class App extends Component {
 
  constructor(props){
     super(props);
-    this.state =({fact:"loading"});
+    this.state =({fact:"loading", encounters: []});
    
   }
 
     componentDidMount(){
     axios.get('https://red-wdp-api.herokuapp.com/api/mars/encounters')
-  .then(function (response) {
-    
-    // console.log(response.data.encounters.length);
-    // console.log(response.data.encounters);
-    let encounters = response.data.encounters;
-
-    for (var i = 0 ; i< encounters.length;i++ ){
-        //console.log( encounters[i]);
-        // document.write(
-        //   "<br>"+"ID : "+encounters[i].id+
-        //   " DATE : "+encounters[i].date+
-        //   " ATYPE : "+encounters[i].atype+
-        //   " ACTION : "+encounters[i].action
-        //   );
-        //document.getElementById('container').innerHTML = "<br>"+"ID : "+encounters[i].id+" DATE : "+encounters[i].date+" ATYPE : "+encounters[i].atype+" ACTION : "+encounters[i].action;
-
-        $( ".container" ).append( 
-          "<br><br>"+"ID : "+encounters[i].id
-          +" DATE : "+encounters[i].date
-          +" ATYPE : "+encounters[i].atype+"<br>"
-          +" ACTION : "+encounters[i].action );
-    }
-
-    // const encComps = encounters.map((encounter)=>{
-    //   return <Encounter id={encounter.id} />
-    // });
-
+    .then((response) => {
+    let data = response.data.encounters;
+    this.setState({encounters: data});
 
   })
 
@@ -54,7 +32,18 @@ class App extends Component {
   }
 
   render(){
+    let encounters = this.state.encounters;
+    let mappedEncounters = encounters.map(encounter => 
+    <div class="getEncounter">
+      <p> {"(Date :) "+encounter.date 
+       +"  (ID :) "+ encounter.id  
+       + " (Atype :) "+ encounter.atype 
+       + " (Action :) "+ encounter.action } </p>
+     </div>
+    
+    );
     return (
+
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
@@ -62,30 +51,16 @@ class App extends Component {
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
+          {mappedEncounters}
         </p>
         <Fact fact={this.state.fact} />
-      <div className="container"></div>  
+        <CheckIn />
+       
       </div>
     );
   }
 }
 
-// class Encounter extends Component {
-//   constructor(props){
-//     super(props);
-//     this.state =({encounter:''});
-//   }
-    
-  
-//   render(){
-//     return(
-//     <div>
-//         <div> {this.props.encounter} </div>
-//     </div>
-//     );
-//   }
-
-// }
 
 class Fact extends Component {
   constructor(props){
@@ -101,5 +76,54 @@ class Fact extends Component {
   }
 
 }
+
+
+//---------------form and input------------------------------
+
+class CheckIn extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {namevalue: '',agevalue: '',jobvalue: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) { //event handler that is called from text field on each click
+  
+    let  theKey = event.target.name + "value";
+    let value = event.target.value;
+
+    console.log(theKey, value);
+    this.setState({[theKey] : value }); //will re-render the box evey time because using setState
+  }
+
+  handleSubmit(event) { //event handler that is called when the form is submited
+    alert('A name was submitted: ' + this.state.namevalue);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+        <p>{this.state.namevalue}</p>
+          Name:
+          <input name="name" type="text" value={this.state.namevalue} onChange={this.handleChange} />
+        </label>
+        <label>
+          Age:
+          <input name="age" type="text" value={this.state.agevalue} onChange={this.handleChange} />
+        </label>
+        <label>
+          Job:
+          <input name="job" type="text" value={this.state.jobvalue} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
+
 
 export default App;
